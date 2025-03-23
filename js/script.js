71f152b214b896b5487b85eca9f5425551e102db
Dawn Kim <70921363+dkim3191@users.noreply.github.com>
2025-03-23 17:57:18 -0400

    mobile nav bar finished. work on desktop nav bar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Disable transitions on page resize
+let resizeTimer;
+window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
+
+const mobileNavToggle = document.querySelector("#mobile-nav-toggle");
+const nav = document.querySelector(".nav-links");
+
+mobileNavToggle.addEventListener("click", () => {
+    mobileNavToggle.classList.toggle("open");
+    nav.classList.toggle("open");
+});
