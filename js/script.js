4394fbd725ef36a6dcae616d7f96ed71bc770da1
Dawn Kim <70921363+dkim3191@users.noreply.github.com>
2025-05-02 22:43:21 -0400

    for korea

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// Disable transitions on page resize
-let resizeTimer;
-window.addEventListener("resize", () => {
-    document.body.classList.add("resize-animation-stopper");
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(() => {
-        document.body.classList.remove("resize-animation-stopper");
-    }, 150);
-});
-
 const mobileNavToggle = document.querySelector("#mobile-nav-toggle");
 const nav = document.querySelector(".nav-links");
 
